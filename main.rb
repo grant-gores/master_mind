d4958c43bf9ed0ea7d0b59da8ed5ffbcea21c84f
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-02-13 20:32:10 -0600

    Complete logic for guessing code 12 times.
    Computer and Human can now try to guess the code
    in 12 turns if they are selected to play the game.
    Have not added code maker feedback yet (pegs).

--- main.rb
+++ main.rb
@@ -3,100 +3,134 @@ require 'colorize'
 class Play
     def start
         player = Player.new
-        Board.new(player)
+        board = Board.new(player)
+        board.solve_code
     end
 end
 
 class Player
     attr_reader :player, :coder
-  
+
     def initialize
-      @player = get_player
-      @coder = get_coder
+        @player = get_player
+        @coder = get_coder
     end
-  
+
     def get_player
-      puts "Who is solving the puzzle this game?\nEnter 'Me' or 'Computer':"
-      loop do
-        input = gets.chomp
-        if input == 'Me'
-          puts "You selected #{input}\nThe computer will make a code for you to solve!\n"
-          return 'Me'
-        elsif input == 'Computer'
-          puts "You selected #{input}\nMake a code for the computer to solve!\n"
-          return 'Computer'
-        else
-          puts "\nTry Again with valid input of 'Me' or 'Computer'\n"
+        puts "Welcome to Mastermind! Go to 'https://www.wikihow.com/Play-Mastermind' for more info"
+        puts "\nWho is solving the puzzle this game?\nEnter 'Me' or 'Computer':"
+        loop do
+            input = gets.chomp
+            if input == 'Me'
+                puts "You selected #{input}\nThe computer will make a code for you to solve!\n"
+                return 'Me'
+            elsif input == 'Computer'
+                puts "You selected #{input}\nMake a code for the computer to solve!\n"
+                return 'Computer'
+            else
+                puts "\nTry Again with valid input of 'Me' or 'Computer'\n"
+            end
         end
-      end
     end
-  
+
     def get_coder
-      if @player == 'Me'
-        return 'Computer'
-      else
-        return 'Me'
-      end
+        @player == 'Me' ? 'Computer' : 'Me'
     end
-end  
+end
 
 class Board
     attr_accessor :master_board, :hint_board
-  
+
     def initialize(player)
-      @player = player.player
-      @master_board = Array.new()
-      @hint_board = []
-      @code = create_code
+        @player = player.player
+        @master_board = []
+        @hint_board = []
+        @code = create_code
     end
 
     def code_values
-      ['red', 'blue', 'green', 'yellow', 'white', 'black']
+        ['red', 'blue', 'green', 'yellow', 'white', 'black']
     end
-      
+
     def hint_values
-      ['white peg','red peg']
+        ['white peg', 'red peg']
     end
 
     def user_generated_code
         puts "\nPlease select 4 colors (in order) to create a code."
-        puts "\nYour color options are: red, blue, green, yellow, white, black"
-        puts "\nExample code: red, blue, blue, yellow\n"
-        
+        puts "Your color options are: red, blue, green, yellow, white, black"
+        puts "Example code: red, blue, blue, yellow\n"
+
         loop do
-          input = gets.chomp.split(",").map(&:strip)
-      
-          # Check if the user entered exactly 4 colors
-          if input.length == 4
-            if input.all? { |color| code_values.include?(color) }
-              puts "The code you selected is: #{input}"
-              return input
+            input = gets.chomp.split(",").map(&:strip)
+
+            if input.length == 4 && input.all? { |color| code_values.include?(color) }
+                puts "The code you selected is: #{input}\n"
+                return input
             else
-              puts "Please enter valid colors from the available options: red, blue, green, yellow, white, black."
+                puts "Invalid input. Enter exactly 4 valid colors."
             end
-          else
-            puts "Enter 4 values for the code!"
-          end
         end
     end
 
     def computer_generated_code
-        puts "\nThe computer will now create a code for you to solve."
         puts "The code has been made, you have 12 guesses!"
-        puts "Remember: 'white peg' means you have one of the correct colors on the board"
-        puts "Remember: 'red peg' means you have the right color and position for one\n"
-        code = code_values.sample(4)
-        return code
+        puts "Example code: red, blue, blue, yellow"
+        puts "Remember: 'white peg' means you have one correct color."
+        puts "Remember: 'red peg' means you have one correct color **and** position.\n"
+        code_values.sample(4) # Generate the code
     end
 
-    def create_code
+    def solve_computer_generated_code
+        attempts = 0
+        while attempts < 12
+            puts "Guess ##{attempts + 1}"
+            input = gets.chomp.split(",").map(&:strip)
+            
+            if input.length == 4 && input.all? { |color| code_values.include?(color) }
+                if input == @code
+                    puts "You solved the code: #{@code} in #{attempts + 1} tries"
+                    return
+                else
+                    puts "Try again."
+                end
+                attempts += 1  # Only increment if a valid guess is made
+            else
+                puts "Invalid guess! Enter exactly 4 valid colors."
+            end
+        end
+        puts "You failed to solve the code."
+        puts "The correct code was #{@code}"
+    end
+
+    def solve_user_generated_code
+        guesses = []
+        12.times do |guess_num|
+            sample = code_values.sample(4)
+            puts "Guess ##{guess_num + 1} -- #{sample}"
+            guesses << sample
+
+            if sample == @code
+                puts "Computer solved the code: #{@code} in #{guess_num + 1} tries!"
+                return
+            else
+                puts "Computer trying again."
+            end
+        end
+        puts "Computer failed to solve the code. The correct code was: #{@code}"
+    end
+
+    def solve_code
         if @player == 'Computer'
-            user_generated_code
+            solve_user_generated_code
         else
-            computer_generated_code
+            solve_computer_generated_code
         end
     end
 
+    def create_code
+        @player == 'Computer' ? user_generated_code : computer_generated_code
+    end
 end
 
 Play.new.start
